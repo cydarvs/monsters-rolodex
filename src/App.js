b1af223e866bc0021154dfb48a3117c3223eea79
Cyfred Odarve <cyfredodarve@gmail.com>
2020-04-10 00:22:46 +0800

    Commit 04102020 12:22AM

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component{
 
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
     };
 
     // bind functions is required if ur not using arrow function method
